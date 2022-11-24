f6cea6911a4bd81a9f637295035b58d59b7189a2
Jaehoon Ahn <ahnza245@gmail.com>
2022-11-25 00:58:35 +0900

    fix checkpoint callback

--- train.py
+++ train.py
@@ -79,7 +79,8 @@ args.default_root_dir = os.path.join("lightning_logs", "full")
 print(args.default_root_dir)
 
 # create the trainer
-trainer = pl.Trainer.from_argparse_args(args, checkpoint_callback=checkpoint_callback)
+trainer = pl.Trainer.from_argparse_args(args, checkpoint_callback=[checkpoint_callback])
+#trainer = pl.Trainer(accelerator='gpu', devices=2, callbacks=[checkpoint_callback])
 
 # setup the dataloaders
 train_datasets = []
